3367c227f16a257cb8734a4f60d2a1634ca4bd0b
K Cartlidge <code@kcartlidge.com>
2017-09-19 21:24:48 +0100

    Added HTTPS with HTTP redirect.

--- cmd/server.go
+++ cmd/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"errors"
 	"fmt"
 	"log"
@@ -9,14 +10,16 @@ import (
 	"time"
 
 	mux "github.com/gorilla/mux"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 // Server ... Handles all sites.
 type Server struct {
-	Port    int
-	Logging bool
-	Router  *mux.Router
-	Server  *http.Server
+	Port      int
+	Logging   bool
+	Hostnames []string
+	Router    *mux.Router
+	Server    *http.Server
 }
 
 // NewServer ... Creates a new server.
@@ -26,15 +29,17 @@ func NewServer(port int, logging bool) (Server, error) {
 	}
 
 	return Server{
-		Port:    port,
-		Logging: logging,
-		Router:  mux.NewRouter(),
-		Server:  nil,
+		Port:      port,
+		Logging:   logging,
+		Hostnames: []string{},
+		Router:    mux.NewRouter(),
+		Server:    nil,
 	}, nil
 }
 
 // AddSite ... Adds a new site to the server.
 func (s *Server) AddSite(hostname, folder string) {
+	s.Hostnames = append(s.Hostnames, hostname)
 	sr := s.Router.Host(hostname).Subrouter()
 	sr.PathPrefix("/").Handler(http.FileServer(http.Dir(folder)))
 }
@@ -44,11 +49,63 @@ func (s *Server) Serve() {
 	addr := strconv.Itoa(s.Port)
 	fmt.Println()
 	fmt.Println("Serving on", addr)
-	s.Server = &http.Server{
-		Handler:      s.Router,
-		Addr:         ":" + addr,
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
+
+	// Only ONE certificate is used so if you are hosting multiple
+	// sites you need a certificate that allows multiple 'common names'.
+	// LetsEncrypt certificates support this.
+	if addr == "443" {
+		// HTTPS
+
+		// Start a standard HTTP server that redirects to HTTPS.
+		fmt.Println("Starting HTTP to HTTPS redirect")
+		go func() {
+			redir := mux.NewRouter()
+			redir.HandleFunc("/", s.HTTP2HTTPS)
+			plain := &http.Server{
+				Handler:      redir,
+				Addr:         ":80",
+				WriteTimeout: 15 * time.Second,
+				ReadTimeout:  15 * time.Second,
+			}
+			plain.ListenAndServe()
+		}()
+
+		// Start the HTTPS server.
+		s.Server = &http.Server{
+			Handler:      s.Router,
+			Addr:         ":" + addr,
+			WriteTimeout: 15 * time.Second,
+			ReadTimeout:  15 * time.Second,
+			TLSConfig:    s.GetTLS(s.Hostnames),
+		}
+		log.Fatal(s.Server.ListenAndServeTLS("", ""))
+	} else {
+		// HTTP
+		s.Server = &http.Server{
+			Handler:      s.Router,
+			Addr:         ":" + addr,
+			WriteTimeout: 15 * time.Second,
+			ReadTimeout:  15 * time.Second,
+		}
+		log.Fatal(s.Server.ListenAndServe())
 	}
-	log.Fatal(s.Server.ListenAndServe())
+}
+
+// GetTLS ... Get TLS support for LetsEncrypt.
+func (s *Server) GetTLS(hostnames []string) *tls.Config {
+	cache := autocert.DirCache("letsencrypt")
+	m := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		Cache:      cache,
+		HostPolicy: autocert.HostWhitelist(hostnames...),
+	}
+	return &tls.Config{GetCertificate: m.GetCertificate}
+}
+
+// HTTP2HTTPS ... Redirects from HTTP to HTTPS.
+func (s *Server) HTTP2HTTPS(w http.ResponseWriter, r *http.Request) {
+	cs := r.Host
+	r.URL.Scheme = "https"
+	r.URL.Host = cs
+	http.Redirect(w, r, r.URL.String(), http.StatusFound)
 }
